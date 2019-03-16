e3af15a918aa56ce5dcca1f49894f11fb9eb43f7
张昆 <1095610623@qq.com>
2019-03-17 01:44:37 +0800

    新增

---
+++ book-fairy-common/src/main/java/com/book/fairy/constant/upload/FileUploadTableEnum.java
@@ -0,0 +1,12 @@
+package com.book.fairy.constant.upload;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yz-he
+ * Date: 2018\2\13 0013 16:52
+ * To change this template use File | Settings | File Templates.
+ * Description: 上传文件项目通途
+ */
+public enum FileUploadTableEnum {
+
+}
---
+++ book-fairy-common/src/main/java/com/book/fairy/utils/ArraysUtlis.java
@@ -0,0 +1,26 @@
+package com.book.fairy.utils;//package com.tangjing.wind.util;
+//
+//import java.util.Arrays;
+//
+///**
+// * Describe:数组工具类
+// * User:tangjing
+// * Date:2017/7/31
+// * Time:上午11:22
+// */
+//
+//public class ArraysUtlis {
+//
+//    /**
+//     * 合并2个数组
+//     * @param array1
+//     * @param array2
+//     * @return
+//     */
+//    public static Object[] mergeArray(Object[] array1, Object[] array2) {
+//        Object[] result = Arrays.copyOf(array1, array1.length + array2.length);
+//        System.arraycopy(array2, 0, result, array1.length, array2.length);
+//        return result;
+//    }
+//
+//}
