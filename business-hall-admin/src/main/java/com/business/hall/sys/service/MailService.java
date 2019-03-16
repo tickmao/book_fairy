f682a3f473a5491a77868361290cd49292747736
张昆 <1095610623@qq.com>
2019-03-17 01:30:53 +0800

    删除

--- business-hall-admin/src/main/java/com/business/hall/sys/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package com.business.hall.sys.service;
-
-import com.business.hall.sys.model.Mail;
-
-import java.util.List;
-
-public interface MailService {
-	void save(Mail mail, List<String> toUser);
-}
