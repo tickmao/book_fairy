392e95a37ea651fd2f123d8996658736ed080101
张昆 <1095610623@qq.com>
2019-03-16 19:56:00 +0800

    架构搭建

---
+++ logs/hall-core.log.sql
@@ -0,0 +1,18 @@
+2019-03-16 16:34:34,736 [http-nio-8083-exec-1] ==>  Preparing: SELECT id AS id,href,chapter,bookname_id AS booknameId FROM SECTION_CHAPTERS WHERE (bookname_id = ?) 
+2019-03-16 16:34:34,863 [http-nio-8083-exec-1] ==> Parameters: 1(String)
+2019-03-16 16:34:34,954 [http-nio-8083-exec-1] <==      Total: 65
+2019-03-16 16:34:44,496 [http-nio-8083-exec-5] ==>  Preparing: SELECT id AS id,href,chapter,bookname_id AS booknameId FROM SECTION_CHAPTERS WHERE (bookname_id = ?) 
+2019-03-16 16:34:44,498 [http-nio-8083-exec-5] ==> Parameters: 1(String)
+2019-03-16 16:34:44,512 [http-nio-8083-exec-5] <==      Total: 65
+2019-03-16 16:43:08,300 [http-nio-8083-exec-28] ==>  Preparing: SELECT id AS id,chapter,content,prev,`next`,section_chapter_id AS sectionChapterId FROM SECTION_DETAILS WHERE (section_chapter_id = ?) 
+2019-03-16 16:43:08,377 [http-nio-8083-exec-28] ==> Parameters: 1(String)
+2019-03-16 16:43:08,401 [http-nio-8083-exec-28] <==      Total: 1
+2019-03-16 16:43:20,049 [http-nio-8083-exec-30] ==>  Preparing: SELECT id AS id,chapter,content,prev,`next`,section_chapter_id AS sectionChapterId FROM SECTION_DETAILS WHERE (section_chapter_id = ?) 
+2019-03-16 16:43:20,051 [http-nio-8083-exec-30] ==> Parameters: 1(String)
+2019-03-16 16:43:20,054 [http-nio-8083-exec-30] <==      Total: 1
+2019-03-16 16:43:28,664 [http-nio-8083-exec-3] ==>  Preparing: SELECT id AS id,chapter,content,prev,`next`,section_chapter_id AS sectionChapterId FROM SECTION_DETAILS WHERE (section_chapter_id = ?) 
+2019-03-16 16:43:28,666 [http-nio-8083-exec-3] ==> Parameters: 2(String)
+2019-03-16 16:43:28,670 [http-nio-8083-exec-3] <==      Total: 1
+2019-03-16 16:43:33,069 [http-nio-8083-exec-8] ==>  Preparing: SELECT id AS id,chapter,content,prev,`next`,section_chapter_id AS sectionChapterId FROM SECTION_DETAILS WHERE (section_chapter_id = ?) 
+2019-03-16 16:43:33,070 [http-nio-8083-exec-8] ==> Parameters: 3(String)
+2019-03-16 16:43:33,075 [http-nio-8083-exec-8] <==      Total: 1
